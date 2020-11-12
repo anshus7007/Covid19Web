59cec543e1c95333dee51f70616b3827ff81f03b
anshus7007 <60043003+anshus7007@users.noreply.github.com>
2020-11-12 23:15:53 +0530

    project completed

--- Covid19App/views.py
+++ Covid19App/views.py
@@ -1,5 +1,18 @@
+import requests
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("My Application")
+    data =True
+    result=None
+    globalSummary=None
+    countries=None
+    while(data):
+        try:
+            result=requests.get("https://api.covid19api.com/summary")
+            globalSummary=result.json()['Global']
+            countries=result.json()['Countries']
+            data=False
+        except:
+            data=True
+    return render(request,'index.html',{'globalSummary':globalSummary,'countries':countries})
